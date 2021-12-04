0897c286c0b9a7f8e7ae446fffba5aca60f43b36
subhash soren <subhashsoren44@gmail.com>
2021-12-04 11:29:28 +0530

    user analytics charts component created

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "February",
+    "Active User": 3000,
+  },
+  {
+    name: "March",
+    "Active User": 2000,
+  },
+  {
+    name: "April",
+    "Active User": 2780,
+  },
+  {
+    name: "May",
+    "Active User": 1890,
+  },
+  {
+    name: "June",
+    "Active User": 2390,
+  },
+  {
+    name: "July",
+    "Active User": 3490,
+  },
+  {
+    name: "August",
+    "Active User": 3490,
+  },
+  {
+    name: "September",
+    "Active User": 4490,
+  },
+  {
+    name: "October",
+    "Active User": 5490,
+  },
+  {
+    name: "November",
+    "Active User": 2490,
+  },
+  {
+    name: "December",
+    "Active User": 1490,
+  },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import './Home.css';
+import {userData} from "../../dummyData";
 
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
-            <Chart />
+            <Chart data={userData} title="User Analytics"  grid dataKey="Active User" />
             
         </div>
     )
