c4fa5588c4f165ef87aed9007e25ae71b24b27be
subhash soren <subhashsoren44@gmail.com>
2021-12-06 13:56:40 +0530

    left widget component created

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 import './WidgetLg.css';
 
 const WidgetLg = () => {
+    const Button = ({type}) => {
+    return <button className={"widgetLgButton" + type}>{type}</button>;
+
+     
+}
+    
     return (
         <div className="widgetLg">
-            large
+            <h3 className="widgetLgTitle">Latest Transactions</h3>
+            <table className="widgetLgTable">
+                <tr className="widgetLgTr">
+                    <th className="widgetLgTh">Customer</th>
+                    <th className="widgetLgTh">Date</th>
+                    <th className="widgetLgTh">Amount</th>
+                    <th className="widgetLgTh">Status</th>
+                </tr>
+                <tr className="widgetLgTr">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Susan Jesus</span>
+                    </td>
+                    <td className="widgetLgDate">6 Nov 2021</td>
+                    <td className="widgetLgAmount">$340.00</td>
+                    <td className="widgetLgStatus">
+                        <Button type="Approved" />
+                    </td>
+                </tr>
+            </table>
         </div>
     )
 }
--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './WidgetSm.css';
+import {Visibility} from '@mui/icons-material';
 
 const WidgetSm = ({title}) => {
     return (
         <div className="widgetSm">
-            small 
+            <span className="widgetSmTitle">New Join Member</span>
+            <ul className="widgetSmList">
+                <li className="widgetSmListItem">
+                    <img src="https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="widgetSmImg" />
+                    <div className="widgetSmUser">
+                        <span className="widgetSmUsername">Lee Wong</span>
+                        <span className="widgetSmUserTitle">Software Analyst</span>
+                    </div>
+                    <button className="widgetSmButton">
+                         Display
+                         <Visibility className="widgetSmIcon" />
+                    
+                    </button>
+                </li>
+            </ul>
             
         </div>
     )
