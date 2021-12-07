a3c5db588aca1503a02e9e885d65d0a4131f3c2b
subhash soren <subhashsoren44@gmail.com>
2021-12-07 18:46:16 +0530

    react-router-dom installed

---
+++ src/pages/userlist/UserList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './UserList.css';
+
+const UserList = () => {
+    return (
+        <div className="userList">
+            userlist
+            
+        </div>
+    )
+}
+
+export default UserList
